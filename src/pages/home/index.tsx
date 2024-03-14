5b4b3599fffb177f005a334321d80f57b0a03d71
Sahilbalhra <sahilbalhra0@gmail.com>
2024-03-15 00:09:48 +0530

    card added to home screen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,65 @@
-import React from "react"
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from "@/components/ui/card";
 
-export default React.memo(Home)
+const Home = () => {
+    return (
+        <div className="grid w-full gap-2 grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <Card className="inline-flex flex-col group w-full">
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+            <Card className="inline-flex flex-col group w-full">
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+            <Card className="inline-flex flex-col group w-full">
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+            <Card className="inline-flex flex-col group w-full">
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
+
+export default Home;
